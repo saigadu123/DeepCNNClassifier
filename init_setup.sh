cbab197ea0c608b47651eb8f095b7284c94e5adc
saikrishna <saikrishnagorantla2001@gmail.com>
2022-09-25 11:47:05 +0530

    add

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START" 
 echo [$(date)]: "creating env with python 3.8 version" 
-conda create --prefix ./saikrishna python=3.8 -y
+conda create --prefix ./krishna python=3.8 -y
 echo [$(date)]: "activating the environment" 
-source activate ./saikrishna
+source activate ./krishna
 echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
 echo [$(date)]: "END" 
