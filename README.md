df8c24bb53644b72b55486a967ab3f2529707157
saikrishna <saikrishnagorantla2001@gmail.com>
2023-04-05 17:08:10 +0530

    setup complete

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ step-2 => Create License file
 
 step-3 => Create template.py file to create all required files in the directory
 
+step-4 => In setup.py file we create the setup
+
+step-5 => In setup.cfg file we create the setup configuration
+
+step-6 => 
+
--- template.py
+++ template.py
@@ -15,7 +15,10 @@ list_of_files = [
     f"src/{package_name}/config/__init__.py",
     f"src/{package_name}/pipeline/__init__.py",
     f"src/{package_name}/entity/__init__.py",
-    f"src/{package_name}/constants/__init__.py"
+    f"src/{package_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
     "configs/config.yaml",
     "dvc.yaml",
     "params.yaml",
@@ -26,7 +29,7 @@ list_of_files = [
     "setup.cfg",
     "pyproject.toml",
     "tox.ini",
-    "research/trials.ipynb",
+    "research/trials.ipynb"
 
 ]
 
