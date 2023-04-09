d375b55a1cf14ef223269ab5f4472805ead77bc7
saikrishna <saikrishnagorantla2001@gmail.com>
2023-04-09 17:53:52 +0530

    stage-02 rsearch done

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating env with python 3.8 version"
-conda create -p env python==3.7 -y
+conda create -p env python==3.8 -y
 echo [$(date)]: "activating the environment"
 source activate ./env
 echo [$(date)]: "installing the dev requirements"
