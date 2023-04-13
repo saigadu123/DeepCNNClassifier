5da33cc038302cd453263044d4c5eb313e966126
saikrishna <saikrishnagorantla2001@gmail.com>
2023-04-13 12:52:59 +0530

    stage-3,stage-4 is added

---
+++ src/deepClassifier/pipeline/stage_03_prepare_callbacks.py
@@ -0,0 +1,24 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import PrepareCallback
+from deepClassifier import logger
+
+STAGE_NAME = "Prepare Call backs stage"
+
+
+def main():
+    config = ConfigurationManager()
+    prepare_callback_config = config.get_prepare_call_back_config()
+    prepare_callback = PrepareCallback(config=prepare_callback_config)
+    callback_list = prepare_callback.get_tb_ckpt_callbacks()
+
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"*********************************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
